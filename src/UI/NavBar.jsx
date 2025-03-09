1dd5b0365c6caa5c5a3e69c9b10e8b5fdefab139
AakashSankarAS <aakashsankar412@gmail.com>
2025-03-09 22:53:42 +0530

    first commit

--- src/UI/NavBar.jsx
+++ src/UI/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-scroll";
+import Money from "../assets/MoneyVerse.png";
 
 function Navbar() {
   return (
     <nav className="w-full top-0 left-0 z-50 bg-gradient-to-r from-blue-500 to-blue-700 text-white shadow-md">
       <div className="container mx-auto flex justify-between items-center p-5">
         <div className="flex items-center text-xl font-bold">
-          <img src="assets\MoneyVerse.png" width={"11%"} height={"11%"} />
+          <img src={Money} width={"11%"} height={"11%"} />
           MoneyVerse
         </div>
         <div className="space-x-6 hidden md:flex">
